af3f83803c7aceae5fda6204c1f57bb2b4d60363
Ross Morgan <ross.morgan933@gmail.com>
2024-10-30 20:30:29 -0500

    completed services

---
+++ Services/TicketRepublicEventServices.cs
@@ -0,0 +1,48 @@
+﻿using SweNamelessBE_RepositoryPattern.Interfaces;
+using SweNamelessBE_RepositoryPattern.Models;
+using SweNamelessBE_RepositoryPattern.DTOs;
+
+namespace SweNamelessBE_RepositoryPattern.Services
+{
+    public class TicketRepublicEventService : ITicketRepublicEventService
+    {
+        private readonly ITicketRepublicEventRepository _eventServicesRepo;
+
+        public TicketRepublicEventService(ITicketRepublicEventRepository eventServicesRepo)
+        {
+            _eventServicesRepo = eventServicesRepo;
+        }
+
+        public async Task<List<Event>> GetEventsAsync()
+        {
+            return await _eventServicesRepo.GetEventsAsync();
+        }
+
+        public async Task<Event> GetEventByIdAsync(int id)
+        {
+            return await _eventServicesRepo.GetEventByIdAsync(id);
+        }
+
+
+        public async Task<List<Event>> GetEventsByUserAsync(string uid)
+        {
+            return await _eventServicesRepo.GetEventsByUserAsync(uid);
+        }
+
+        public async Task<Event> PostEventAsync(CreateEventDTO eventDTO)
+        {
+            return await _eventServicesRepo.PostEventAsync(eventDTO);
+        }
+
+        public async Task<Event> UpdateEventAsync(int id, UpdateEventDTO eventDTO)
+        {
+            return await _eventServicesRepo.UpdateEventAsync(id, eventDTO);
+
+        }
+        public async Task<Event> DeleteEventAsync(int id)
+        {
+            return await _eventServicesRepo.DeleteEventAsync(id);
+        }
+    }
+}
+
---
+++ Services/TicketRepublicRSVPServices.cs
@@ -0,0 +1,29 @@
+﻿using SweNamelessBE_RepositoryPattern.Interfaces;
+using SweNamelessBE_RepositoryPattern.Models;
+using SweNamelessBE_RepositoryPattern.DTOs;
+
+namespace SweNamelessBE_RepositoryPattern.API.Services
+{
+    public class TicketRepublicRSVPService : ITicketRepublicRSVPService
+    {
+        private readonly ITicketRepublicRSVPRepository _ticketRepublicRSVPRepository;
+        public TicketRepublicRSVPService(ITicketRepublicRSVPRepository ticketRepublicRSVPRepository)
+        {
+            _ticketRepublicRSVPRepository = ticketRepublicRSVPRepository;
+        }
+
+        public async Task<List<RSVP>> GetRSVPsAsync(string uid)
+        {
+            return await _ticketRepublicRSVPRepository.GetRSVPsAsync(uid);
+        }
+
+        public async Task<RSVP> PostRSVPAsync(RSVP rsvp)
+        {
+            return await _ticketRepublicRSVPRepository.PostRSVPAsync(rsvp);
+        }
+        public async Task<RSVP> DeleteRSVPAsync(int id)
+        {
+            return await _ticketRepublicRSVPRepository.DeleteRSVPAsync(id);
+        }
+    }
+}
